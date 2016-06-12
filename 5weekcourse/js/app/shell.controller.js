5f6c8793a9ce2da194ec355591f456c01367eeed
coreyshuman <ctshumancode@gmail.com>
2016-06-12 09:38:02 -0700

    Added links

--- 5weekcourse/js/app/shell.controller.js
+++ 5weekcourse/js/app/shell.controller.js
@@ -9,7 +9,7 @@
             vm.instructor = "Corey Shuman";
             
             vm.slackLink = 'https://geekwiserobotics.slack.com';
-            vm.githubLink = 'github.com';
+            vm.githubLink = 'http://coreyshuman.github.io/GeekwiseRobotics/5weekcourse';
             vm.labLink = 'https://123d.circuits.io/lab';
         }
 })();
